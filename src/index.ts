9c1c640cca3fca3986b7c016209f9b28c8a7c696
ajiken4610 <86188770+ajiken4610@users.noreply.github.com>
2022-01-15 17:22:34 +0900

    PWA化の実験

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Parser } from "../lib/ams/ams";
+import { Parser } from "./lib/ams/ams";
 
 console.log(Parser);
 console.log("Hello World!");
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+const WebpackWPAManifest = require("webpack-pwa-manifest");
 
 const isProduction = process.env.NODE_ENV == "production";
 
@@ -60,6 +61,16 @@ module.exports = () => {
   if (isProduction) {
     config.mode = "production";
 
+    config.plugins.push(new WebpackWPAManifest({
+      short_name: "AMS",
+      name: "AjiMarkupScript",
+      description: "This is a demo of AMS.",
+      crossorigin: 'use-credentials',
+      icons: [{
+        src: path.resolve("src/image/logo.png"),
+        sizes: [96, 128, 192, 256, 384, 512],
+      }]
+    }));
     config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
   } else {
     config.mode = "development";
